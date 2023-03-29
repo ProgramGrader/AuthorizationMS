0b307f3e8438b1b9791cffc99432a1249ce16970
Edwrld <edwardutria12@gmail.com>
2023-03-29 09:09:03 -0400

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,13 +23,13 @@ terraform {
       version = "~> 4.28"
     }
   }
-#  backend "s3" { #not compatible with cloud
-#    bucket = "tfstate-3ea6z45i"
-#    key    = "AuthorizationMS/key"
-#    region = "us-east-2"
-#    dynamodb_table = "app-state"
-#    encrypt = true
-#  }
+  backend "s3" { #not compatible with cloud
+    bucket = "tfstate-3ea6z45i"
+    key    = "AuthorizationMS/key"
+    region = "us-east-2"
+    dynamodb_table = "app-state"
+    encrypt = true
+  }
   
 }
 
@@ -41,25 +41,25 @@ locals {
   }
 }
 
-#provider "aws" {
-#  alias  = "primary"
-#  region = var.primary_aws_region
-#  access_key = var.aws_access_key
-#  secret_key = var.aws_secret_key
-#
-#  default_tags {
-#    tags = local.shared_tags
-#  }
-#}
-#
-#provider "aws" {
-#  alias  = "secondary"
-#  region = var.secondary_aws_region
-#
-#  default_tags {
-#    tags = local.shared_tags
-#  }
-#}
+provider "aws" {
+  alias  = "primary"
+  region = var.primary_aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
+  default_tags {
+    tags = local.shared_tags
+  }
+}
+
+provider "aws" {
+  alias  = "secondary"
+  region = var.secondary_aws_region
+
+  default_tags {
+    tags = local.shared_tags
+  }
+}
 
 data "aws_kms_key" "coreKmsENDECKeyByAlias" {
   key_id = "alias/EDKey"
