03ed6aa4f742dc3ad41cc44d0f1ab32eecf3405d
Edwrld <edwardutria12@gmail.com>
2023-03-28 22:28:33 -0400

    passing needed credentials to aws provided

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,27 +8,19 @@
 
 terraform {
 
-  cloud{
-
-    organization = "zacclifton"
-    workspaces {
-        name = "AuthorizationMS-dev"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.28"
     }
   }
-#  backend "s3" { #when using
-#    bucket = "tfstate-3ea6z45i"
-#    key    = "AuthorizationMS/key"
-#    region = "us-east-2"
-#    dynamodb_table = "app-state"
-#    encrypt = true
-#  }
+  backend "s3" { #when using
+    bucket = "tfstate-3ea6z45i"
+    key    = "AuthorizationMS/key"
+    region = "us-east-2"
+    dynamodb_table = "app-state"
+    encrypt = true
+  }
   
 }
 
@@ -43,6 +35,8 @@ locals {
 provider "aws" {
   alias  = "primary"
   region = var.primary_aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
   default_tags {
     tags = local.shared_tags
